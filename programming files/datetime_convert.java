12c1bb4edcb1d7580810b1523df2f3b3d823e686
Mish Wilson <112350999+mishwilso@users.noreply.github.com>
2023-03-20 18:06:39 -0400

    Extermination Complete

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-1710:45:30";
+        String dateStr = "2022-03-17 10:45:30";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
