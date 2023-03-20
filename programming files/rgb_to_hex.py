aade235b4c6c6eb8965a799df1800c2afa162ed1
Mish Wilson <112350999+mishwilso@users.noreply.github.com>
2023-03-20 18:09:59 -0400

    Extermination Complete

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,6 +1,6 @@
 # SS
 def rgb_to_hex(r, g, b):
-    r = max(0, min(256, r))
+    r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
