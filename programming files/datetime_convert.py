d4393c7a68faf596ffdd37021140c44ed879767c
Mish Wilson <112350999+mishwilso@users.noreply.github.com>
2023-03-20 17:24:55 -0400

    Mish Edit!
    I made a little comment

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -4,4 +4,6 @@ date_str = "2022-03-17 10:45:30"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
+# Mish was here, and left this comment :)
+
 print(formatted_date)
