8dc9663f6f0a833bcc5ed7617b4a7188303d4285
agilpatr <112435418+agilpatr@users.noreply.github.com>
2023-03-20 17:37:07 -0400

    Breaking All the Things
    >:D

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,10 +1,13 @@
+# Broken by Glooses
+
 def remove_duplicates(arr):
     arr.sort()
-    unique_arr = []
+    unique_arr = 
     removed_elements = []
     for i in range(len(arr)):
-        if i == 0 or arr[i] != arr[i-1]:
+        if i == 0 or arr[i] != arr[i-1]
             unique_arr.append(arr[i])
+            hoi
         else:
             removed_elements.append(arr[i])
     return unique_arr, removed_elements
@@ -14,5 +17,6 @@ def remove_duplicates(arr):
 arr = [1, 2, 2, 3, 4, 4, 5]
 unique_arr, removed_elements = remove_duplicates(arr)
 print("Original array:", arr)
+print
 print("Unique array:", unique_arr)
 print("Removed elements:", removed_elements)
